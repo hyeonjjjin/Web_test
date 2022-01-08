0960b902f7a900bcc392658de811e16371145f04
jahj30734 <jahj30734@gmail.com>
2022-01-08 19:12:30 +0900

    0108 새 코드 추가할건데  망할까봐 백업

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import EnSlogo from './EnSlogo.png';
 import './App.css';
 
 function App() {
-  
-
   // destructuring 이라는 문법임. array, object에 있던 자료를 변수에 쉽게 담고싶을 때
   // var [a, b] = [10, 100];
 
@@ -14,9 +12,19 @@ function App() {
   let [likes, likesFunChange] = useState(0);
 
   let [modal, modal변경] = useState(false); // 모달창을 켜고닫는 스위치 
+  let [clickedLineN, clickedLineN변경] = useState(0);
+  function 반복된UI(){
+
+    var array = [];
+    for (var i=0;i<3;i++){
+      array.push(<div>안녕</div>);
+    }
+    return array
+  }
+
+
 
   var arr = [2,3,4];
-
   //map 함수, 반복문. 결과가 새로운 array로 반환  
   var newarr = arr.map(function(a){
     return a*2
@@ -49,15 +57,9 @@ function App() {
   
   function Modal창띄우기(){
     {
-      modal == false
-      ? modal변경(true)
-      : modal변경(false)
-
-      modal == true 
-      ? <Modaltest/> 
-      : null
-
+      modal == false? modal변경(true):modal변경(false)
     }
+    clickedLineN변경(search(a))
   }
 
   let designLogo = {color: 'white', fontSize: '25px'};
@@ -75,11 +77,11 @@ function App() {
      <img src={EnSlogo} id='logo'/>
     
      {
-      //  a에는 리스트 요소 하나하나가 들어감
-       글제목.map(function(a){
+      //  a에는 리스트 요소 하나하나가 들어감 i에는 index
+       글제목.map(function(a, i){
          return (
           <div className="list">
-            <h3 onClick={()=>{modal == false? modal변경(true):modal변경(false)}}>{a}   </h3>
+            <h3 onClick={()=>{clickedLineN변경(i)}}>{a}   </h3>
             <span onClick={()=>{likesFunChange(likes+1)}}>👍</span>{likes} 
             <p>{date}</p>
             <hr/>
@@ -87,17 +89,23 @@ function App() {
          )
        })
      }
-
+    
 
       {/* 안에 if는 쓸 수 없음 */}
      {
        modal == true
-       ? <Modal/>
+       // 자식노드, 그니까 Modal 컴포넌트에서 가져다 쓰고싶은 App 컴포넌트 내의 데이터를 여기서 넘겨줘. 작명={전송할스테이트} 형식으로
+       ? <Modal lineName = {글제목} clickedLineN = {clickedLineN}></Modal>
        : null
      }
      <button  onClick={제목바꾸기}>제목 수정버튼</button>
      <button  onClick={순서바꾸기}>순서 재배치버튼</button>
-     <button  onClick={Modal창띄우기}>Modal창 띄우기 버튼</button>
+     <button  onClick={()=>modal변경(!modal)}>Modal창 띄우기 버튼</button>
+
+     <button onClick={()=>{clickedLineN변경(0)}}>버튼1</button>
+     <button onClick={()=>{clickedLineN변경(1)}}>버튼2</button>
+     <button onClick={()=>{clickedLineN변경(2)}}>버튼3</button>
+
     </div>
 
   );
@@ -115,12 +123,12 @@ function Modaltest (){
   
 }
 
-// component만들고 원하는 HTML담기 관습적으로 대문자로 시작.
-function Modal(){
+// component만들고 원하는 HTML담기 관습적으로 대문자로 시작. 위에서 전해준 데이터들 다 props 내에 들어있어
+function Modal(props){
   return(
     // return 안에 <div>를 병렬적으로 쓸 수 없음. 쓰려면 <div> 로 묶어야하는데 의미없는 div를 쓰고싶지 않다면 <></>로 묶을 수 있음
   <div className="modal">
-    <h2>제목</h2>
+    <h2>{props.lineName[props.clickedLineN]}</h2>
     <p>날짜</p>
     <p>상세내용</p>
 </div>
